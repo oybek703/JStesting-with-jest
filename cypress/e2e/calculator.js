caf81784910e5a05718135442a0257f534b678fc
oybek703 <hhoybek@gmail.com>
2021-03-14 03:05:17 +0500

    cypress configuration added

---
+++ cypress/e2e/calculator.js
@@ -0,0 +1,11 @@
+describe('anonymous calculator', () => {
+  it('should make calculations', function() {
+    cy.visit('/')
+      cy.findByText(/^1$/).click()
+      cy.findByText(/^\+$/).click()
+      cy.findByText(/^2$/).click()
+      cy.findByText(/^=$/).click()
+      cy.findByTestId('title')
+      .should('have.text', '3')
+  })
+})
