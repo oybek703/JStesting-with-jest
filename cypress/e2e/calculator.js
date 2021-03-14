f1f1f2fbb86c08d0d19473f014971b409dbd4776
oybek703 <hhoybek@gmail.com>
2021-03-14 17:50:13 +0500

    e2e testing done on calculator project with cypress

--- cypress/e2e/calculator.js
+++ cypress/e2e/calculator.js
@@ -8,3 +8,17 @@ describe('anonymous calculator', () => {
       cy.findByTestId('total').should('have.text', '3')
   })
 })
+
+describe('authenticated user', () => {
+  it('should display authenticated user', function() {
+    cy.createUser()
+      .then(user => {
+        cy.findByText(/login/i).click()
+        cy.submitForm(user)
+        cy.findByTestId('username-display').should('have.text', user.username)
+        cy.findByText(/logout/i).click()
+        cy.findByTestId('username-display').should('not.exist')
+    })
+
+  })
+})
